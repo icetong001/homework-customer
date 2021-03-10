addf7ae2b6844d2e0d8f6e9e162862f4eb28abf5
icetong001 <doralee1005@gmail.com>
2021-03-10 15:55:34 +0800

    something go wrong

--- src/main/java/com/tong/Money.java
+++ src/main/java/com/tong/Money.java
@@ -1,18 +1,27 @@
 package com.tong;
 
 public class Money {
-    public static void main(String[] args) {
-        Customer cus1 = new Customer(0013, 490, 0, 490);
-        Customer cus2 = new Customer(0052, 1000, 100, 900);
-        Customer cus3 = new Customer(0081, 290, 0, 290);
-        Customer cus4 = new Customer(2122, 2000, 200, 1800);
+//    public static void main(String[] args) {
+//        Customer cus1 = new Customer("0013",  490, 0,   490);
+//        Customer cus2 = new Customer("0052", 1000, 100,  900);
+//        Customer cus3 = new Customer("0081",  290,   0,   290);
+//        Customer cus4 = new Customer("2122", 2000, 200, 1800);
 
 
-        cus1.print();
-        cus2.print();
-        cus3.print();
-        cus4.print();
-    }
+//        cus1.print();
+//        cus2.print();
+//        cus3.print();
+//        cus4.print();
+//    }
 
 
-}
+    Customer[] cus = {
+            new Customer("0013", 490, 0, 490),
+            new Customer("0052", 1000,100 , 900),
+            new Customer("0081", 290, 0, 290),
+            new Customer("2122", 2000, 200, 1800)
+    };
+    //for (int i = 0; i < cus.length; i++) {
+//        cus[i].print();
+
+}//}
